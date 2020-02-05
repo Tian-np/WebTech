bfd9e3b039c45ed0013e15ab609e4b7abd48cb3d
Naphat Pornbunruang <Naphat4566@gmail.com>
2020-02-05 23:00:24 +0700

    WebTech

---
+++ main.js
@@ -0,0 +1,47 @@
+
+function site() {
+  var txt = $("#txt_1").val();
+  window.open("https://www.google.com/search?q=site: " + txt);
+}
+function inurl() {
+  var txt = $("#txt_2").val();
+  window.open("https://www.google.com/search?q=inurl: " + txt);
+}
+function filetype() {
+  var txt = $("#txt_3").val();
+  window.open("https://www.google.com/search?q=filetype: " + txt);
+}
+function define() {
+  var txt = $("#txt_4").val();
+  window.open("https://www.google.com/search?q=define: " + txt);
+}
+function related() {
+  var txt = $("#txt_5").val();
+  window.open("https://www.google.com/search?q=related: " + txt);
+}
+function etymology() {
+  var txt = $("#txt_6").val();
+  window.open("https://www.google.com/search?q=etymology: " + txt);
+}
+function intitle() {
+  var txt = $("#txt_7").val();
+  window.open("https://www.google.com/search?q=intitle: " + txt);
+}
+function nearby() {
+  var txt = $("#txt_8").val();
+  window.open("https://www.google.com/search?q=" + txt + " nearby");
+}
+
+
+function searchp() {
+  var txt = $("#txt_p").val();
+  window.open("https://www.google.com/search?q=" + txt);
+}
+function searchm() {
+  var txt = $("#txt_m").val();
+  window.open("https://www.google.com/search?q=" + txt);
+}
+function searcht() {
+  var txt = $("#txt_t").val();
+  window.open("https://www.google.com/search?q=" + txt);
+}
